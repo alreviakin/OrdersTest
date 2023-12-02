569a89474f863d2d5954a8a6d063dd24ac67949b
aleksejrevakin <alexey.amahasla@gmail.com>
2023-12-02 22:29:40 +0300

    Create mapsController

--- OrdersTest/Maps/Controller/MapsViewController.swift
+++ OrdersTest/Maps/Controller/MapsViewController.swift
@@ -8,22 +8,16 @@
 import UIKit
 
 class MapsViewController: UIViewController {
+    
+    private let mapsView = MapsView()
+    
+    override func loadView() {
+        self.view = mapsView
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
