0aad2cf6e14e3d6cae12eb8bfa256f499d097779
aleksejrevakin <alexey.amahasla@gmail.com>
2023-12-03 19:20:16 +0300

    Update managers

---
+++ OrdersTest/Manager/GoogleMapManager.swift
@@ -0,0 +1,17 @@
+//
+//  GoogleMapManager.swift
+//  OrdersTest
+//
+//  Created by Алексей Ревякин on 03.12.2023.
+//
+
+import Foundation
+//import GoogleMaps
+//
+//class GoogleMapManagee {
+//    static let shared = GoogleMapManagee()
+//    
+//    private init() {}
+//    
+//    private let apiKey = "AIzaSyAAy21mr_M6HtfDUMAzuP9Sgy1mToG4gHg"
+//}
--- OrdersTest/Maps/View/MapsView.swift
+++ OrdersTest/Maps/View/MapsView.swift
@@ -7,8 +7,43 @@
 
 import Foundation
 import UIKit
+//import GoogleMaps
+
 
 
 class MapsView: BaseMainView {
-    
+//    private var mapView: GMSMapView = {
+//        
+//        let camera = GMSCameraPosition.camera(withLatitude: -33.8683, longitude: 151.2086, zoom: 6)
+//        let map = GMSMapView(frame: .zero, camera: camera)
+//        let marker = GMSMarker()
+//        marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
+//        marker.title = "Sydney"
+//        marker.snippet = "Australia"
+//        marker.map = map
+//        return map
+//    }()
+//    
+//    override init(frame: CGRect) {
+//        super.init(frame: frame)
+//        configre()
+//    }
+//    
+//    required init?(coder: NSCoder) {
+//        fatalError("init(coder:) has not been implemented")
+//    }
+//    
+//    private func configre() {
+//        addSubview(mapView)
+//    }
+//    
+//    override func layoutSubviews() {
+//        super.layoutSubviews()
+//        NSLayoutConstraint.activate([
+//            mapView.topAnchor.constraint(equalTo: topAnchor),
+//            mapView.leadingAnchor.constraint(equalTo: leadingAnchor),
+//            mapView.trailingAnchor.constraint(equalTo: trailingAnchor),
+//            mapView.bottomAnchor.constraint(equalTo: bottomAnchor),
+//        ])
+//    }
 }
